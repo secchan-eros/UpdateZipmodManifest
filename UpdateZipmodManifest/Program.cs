d006ee19d44ec77607deb5d5b5939d53672e3052
Kazuki Takahashi <seta666999@gmail.com>
2021-09-01 00:21:52 +0900

    プロジェクト ファイルを追加します。

---
+++ UpdateZipmodManifest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdateZipmodManifest
+{
+    class Program
+    {
+        const string MANIFEST_FILE_NAME = "manifest.xml";
+#if DEBUG
+        const string PATH = @"D:\KoikatsuSunshine\mods";
+#else
+        const string PATH = @".\\mods";
+#endif
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"置換対象フォルダ {Path.GetFullPath(PATH)}");
+            Console.WriteLine("Enterキーで置換開始 (Ctrl+Cで中断)");
+            Console.ReadLine();
+            Console.WriteLine("置換開始");
+            try
+            {
+                var filePaths = Directory.GetFiles(PATH, "*.zipmod", SearchOption.AllDirectories);
+                foreach (var path in filePaths)
+                {
+                    using (var archive = ZipFile.Open(path, ZipArchiveMode.Update))
+                    {
+                        var entry = archive.Entries.FirstOrDefault((e) => e.FullName.Equals(MANIFEST_FILE_NAME));
+                        if (entry != null)
+                        {
+                            string original;
+                            using (var sr = new StreamReader(entry.Open(), Encoding.UTF8))
+                            {
+                                original = sr.ReadToEnd();
+                            }
+
+                            var replaced = original.Replace("<game>Koikatsu</game>", "<game>Koikatsu Sunshine</game>");
+
+                            if (original != replaced)
+                            {
+                                entry.Delete();
+
+                                var newEntry = archive.CreateEntry(MANIFEST_FILE_NAME);
+                                using (var sw = new StreamWriter(newEntry.Open(), Encoding.UTF8))
+                                {
+                                    sw.Write(replaced);
+                                }
+
+                                Console.WriteLine($"Replaced {path}");
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine("置換完了");
+                Console.WriteLine("");
+                Console.WriteLine("終了するにはキーを押してください...");
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ UpdateZipmodManifest/readme.md
@@ -0,0 +1,20 @@
+﻿# zipmod game名置換アプリ
+
+## 概要
+
+zipmodのmanifest.xmlで指定されている `game` を、 `Koikatsu` から `Koikatsu Sunshine` に置換します。
+
+## 必要環境
+
+.NET4.6
+
+## 使用方法
+
+1. KoikatsuSunshineのインストールフォルダに UpdateZipmodManifest.exeを移動、もしくはコピーします。
+2. UpdateZipmodManifest.exe を実行します。
+3. 表示される置換対象フォルダのパスを確認し、Enterを押します。
+4. 置換完了後 UpdateZipmodManifest.exe を削除します。
+
+## 免責事項
+
+アプリ作成者は、このアプリの使用によって生じたいかなる損害についても一切責任を負いません。
